0373866674c10128e24342b4af07b492e2f54bb2
Ichooo <arnesson.pontus@gmail.com>
2020-02-08 19:20:28 +0100

    Add Google analytics

--- src/App.js
+++ src/App.js
@@ -14,8 +14,12 @@ import Sponsors from "./components/Sponsors";
 import Footer from "./components/Footer";
 
 import { Route, HashRouter as Router } from "react-router-dom";
+import ReactGA from "react-ga";
 
-function App() {
+const App = () => {
+  ReactGA.initialize("UA-158085908-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <Router>
@@ -34,6 +38,6 @@ function App() {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
