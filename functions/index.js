456b01157601e5681300b71c5aab34a35f252d22
Pontus Arnesson <arnesson.pontus@gmail.com>
2020-06-15 21:58:01 +0200

    Test with google recaptcha - not complete

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require("firebase-functions");
+const axios = require("axios");
+const cors = require("cors");
+const corsHandler = cors({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.set("Access-Control-Allow-Origin", "*").sendStatus(200);
-  response.send("Hello from Firebase!");
+  corsHandler(request, response, async () => {
+    response.set("Access-Control-Allow-Origin", "*");
+    response.send("Hello from Firebase!");
+    /*
+    const secret = "<server-site-key>";
+    const token = request.body.value;
+    console.log(request.body.value);
+
+    axios
+      .post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`
+      )
+      .then((res) => {
+        console.log("thennnnn");
+        return response.status(200).send({ res });
+      })
+      .catch((err) => {
+        console.log("catchhh");
+        console.log(err);
+        return response.status(400).send("näääääätverk");
+      });
+      */
+  });
 });
