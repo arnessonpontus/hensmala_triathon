e3c35078cac31486c58e63b2f8d6f40ef5a7c091
Pontus Arnesson <arnesson.pontus@gmail.com>
2022-05-25 22:37:07 +0200

    Add article

--- src/components/media/Articles.js
+++ src/components/media/Articles.js
@@ -13,7 +13,7 @@ class Articles extends React.Component {
     isDropdownOpen: false,
   };
 
-  prevYears = ["2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013"];
+  prevYears = ["2021","2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013"];
 
   onYearTap = (year) => {
     document
@@ -63,9 +63,9 @@ class Articles extends React.Component {
           </DropdownMenu>
         </ButtonDropdown>
         <div className="px-5 pb-5">
-          <h3>2021</h3>
+          <h3>2022</h3>
           <Row>
-            {articles["2021"].map((article) => {
+            {articles["2022"].map((article) => {
               return <ArticleSection article={article} />;
             })}
           </Row>
