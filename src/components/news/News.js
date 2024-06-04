0e50826e17000e83a4272de67a16c74b65d32c8d
Pontus <arnesson.pontus@gmail.com>
2024-06-04 20:24:55 +0200

    Add session storage for keep track of num news viewed

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,24 +1,24 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { Button } from "reactstrap";
 import { Col } from "reactstrap";
 import newsArc from "../../assets/news";
 import OneNews from "./OneNews";
-
+import UseNewsTracker from "../../hooks/UseNewsTracker";
 
 const News = (props) => {
     const chunkSize = useRef(6)
-    const [currentOffset, setCurrentOffset] = useState(chunkSize.current);
+    const [newsCount, setNewsCount] = UseNewsTracker(chunkSize.current);
 
     return (
       <Col className="s mt-5">
         <h2>Nyheter</h2>
-        {newsArc.slice(0, currentOffset).map((news, i) => {
+        {newsArc.slice(0, newsCount).map((news, i) => {
           return <OneNews key={i} news={news} />;
         })}
         <div className="d-flex flex-column align-items-center">
-        Visar {Math.min(currentOffset, newsArc.length)} av {newsArc.length}
-        {currentOffset <= newsArc.length &&
-          <Button className="mt-2" onClick={() => setCurrentOffset(currentOffset + chunkSize.current)}>Ladda {Math.min(chunkSize.current, newsArc.length - currentOffset)} till</Button>
+        Visar {Math.min(newsCount, newsArc.length)} av {newsArc.length}
+        {newsCount <= newsArc.length &&
+          <Button className="mt-2" onClick={() => setNewsCount(newsCount + chunkSize.current)}>Ladda {Math.min(chunkSize.current, newsArc.length - newsCount)} till</Button>
         }
         </div>
       </Col>
