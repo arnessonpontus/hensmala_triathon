9161c4038d1c26c576fa4b46b19b6e97401daa34
Pontus <arnesson.pontus@gmail.com>
2024-11-20 21:49:04 +0100

    Format

--- src/features/register/service/checkoutService.ts
+++ src/features/register/service/checkoutService.ts
@@ -5,33 +5,33 @@ import { DEFAULT_CONTACT_EMAIL } from '../../../Constants';
 const stripePromise = loadStripe(`${import.meta.env.VITE_STRIPE_PUBLIC}`);
 
 export const handleCheckout = async (
-    registrationType: "registration-fee-solo" | "registration-fee-team",
-    shirts: Shirt[],
-    numCaps: number,
-    showErrorModal: (message: string, title: string) => void
+  registrationType: "registration-fee-solo" | "registration-fee-team",
+  shirts: Shirt[],
+  numCaps: number,
+  showErrorModal: (message: string, title: string) => void
 ) => {
-    try {
-        const response = await fetch('/.netlify/functions/payment/payment', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ registrationType, shirts, numCaps }),
-        });
+  try {
+    const response = await fetch('/.netlify/functions/payment/payment', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ registrationType, shirts, numCaps }),
+    });
 
-        if (!response.ok) {
-            console.error("Failed to create checkout session");
-            return;
-        }
-
-        const { id: sessionId } = await response.json();
-        const stripe = await stripePromise;
-
-        if (stripe) {
-            await stripe.redirectToCheckout({ sessionId });
-        } else {
-            console.error("Stripe failed to initialize");
-        }
-    } catch (error) {
-        showErrorModal(`Försök igen eller kontakta ${DEFAULT_CONTACT_EMAIL} om felet kvarstår.`, "Kunde inte slutföra betalning")
-        console.error(error)
+    if (!response.ok) {
+      console.error("Failed to create checkout session");
+      return;
     }
+
+    const { id: sessionId } = await response.json();
+    const stripe = await stripePromise;
+
+    if (stripe) {
+      await stripe.redirectToCheckout({ sessionId });
+    } else {
+      console.error("Stripe failed to initialize");
+    }
+  } catch (error) {
+    showErrorModal(`Försök igen eller kontakta ${DEFAULT_CONTACT_EMAIL} om felet kvarstår.`, "Kunde inte slutföra betalning")
+    console.error(error)
+  }
 };
