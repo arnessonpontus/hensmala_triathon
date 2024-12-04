533144b41bd912b8999ab2a461c418a2fef6fdf1
Pontus <arnesson.pontus@gmail.com>
2024-12-04 21:21:24 +0100

    Change name of OrderShirt to MerchOrder

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -12,7 +12,7 @@ import { Sponsors } from "./features/sponsor/pages/Sponsors";
 import { Route, Routes, useLocation } from "react-router-dom";
 import CoronaEdition from "./features/challenge/pages/CoronaEdition";
 import { Fortrampet } from "./features/fortrampet/pages/Fortrampet";
-import { OrderShirt } from "./features/register/pages/OrderShirt";
+import { OrderShirt } from "./features/register/pages/MerchOrder";
 import PaymentCancelled from "./features/register/pages/PaymentCancelled";
 import PaymentSuccess from "./features/register/pages/PaymentSuccess";
 import { useEffect } from "react";
