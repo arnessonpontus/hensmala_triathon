da9712d97005a2b7bc14a16fb9e9688fc359c13a
AndreArnesson <andre.arnesson@gmail.com>
2025-04-01 22:33:35 +0200

    changed youtube video

--- src/features/register/pages/PaymentSuccess.tsx
+++ src/features/register/pages/PaymentSuccess.tsx
@@ -45,7 +45,7 @@ const PaymentSuccess: React.FC = () => {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/xiAz4cuyETY"
+          src="https://www.youtube.com/watch?v=ZLqLqEJarTA"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
